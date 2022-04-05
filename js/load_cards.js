4b24a315df8d07d0f9ff8988aa0746b5f87c8060
Pau Fernández <pau.fernandez@upc.edu>
2022-04-05 11:07:54 +0200

    Utilizo módulos ESM y pulo detalles

--- js/load_cards.js
+++ js/load_cards.js
@@ -1,6 +1,10 @@
+import { printCards } from './print_cards.js';
+import dataProjects from './data_projects.js';
+
 export function loadCards() {
+  let DOMcardsProjects = document.getElementById("DOMcardsProjects");
   DOMcardsProjects.innerHTML = "";
-  data_projects.forEach((data, i) => printCards(data, i));
+  dataProjects.forEach((data, i) => printCards(data, i));
 }
 
-export const saludo = "hola , soy un modulo";
+export const saludo = "Hola, soy Davina y lo peto!";
--- js/print_cards.js
+++ js/print_cards.js
@@ -1,4 +1,5 @@
 export function printCards(data) {
+  let DOMcardsProjects = document.getElementById("DOMcardsProjects");
   DOMcardsProjects.innerHTML += `
     <section class="card">
       <h3>${data.title}</h3>
@@ -12,3 +13,18 @@ export function printCards(data) {
     </section>
   `
 }
+
+/*
+
+<section class="card">
+  <h3>Proyecto</h3>
+  <div ><img  class="card_img"src="/assets/img/avatarDavina.png" alt=""></div>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque labore iure iusto perferendis, rerum mollitia voluptatibus eum molestias delectus officiis, atque quaerat quisquam ratione doloribus. Eos vero officia omnis est.
+  </p>
+  <p><b>Skills:</b> html, css, js </p>
+  <a href="#" class="card__btn">Source </a>
+  <a href="#" class="card__btn optional">Demo </a>
+</section>
+
+*/
